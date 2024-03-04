15c42dccbe2d7f5e6801ec89d2e4bdb6a48c61fd
firecodes <384502958@qq.com>
2024-03-04 17:22:38 +0800

    add

---
+++ src/pages/category/custom.vue
@@ -0,0 +1,33 @@
+<template>
+    <nut-category @change="changeCustom">
+        <nut-category-pane type="custom" :customCategory="customCategory" @onChange="changeCustom">
+        </nut-category-pane>
+    </nut-category>
+</template>
+
+<script lang="ts">
+import { categoryInfo, categoryChild, customCategory } from './data.json';
+
+export default {
+    setup() {
+        const data = reactive({
+            customCategory: [{}],
+        });
+        onMounted(() => {
+            setTimeout(() => {
+                getData();
+            }, 500);
+        });
+        const getData = () => {
+            console.log('点击getData')
+        };
+        const changeCustom = () => {
+            console.log('点击分类数据')
+        };
+        return {
+            changeCustom,
+            ...toRefs(data)
+        }
+    }
+};
+</script>
--- src/pages/category/index.vue
+++ src/pages/category/index.vue
@@ -1,7 +1,54 @@
-<script lang="ts" setup></script>
-
 <template>
-    <view> 页面模板,新建pages,将此页面内容复制粘贴到新建.vue文件 </view>
+    <AppProvider>
+        <nut-category :category="category" @change="change">
+            <nut-category-pane :categoryChild="categoryChild" @onChange="onChange">
+            </nut-category-pane>
+            <nut-category-pane type="custom" :customCategory="customCategory" @onChange="changeCustom">
+            </nut-category-pane>
+        </nut-category>
+    </AppProvider>
 </template>
 
-<style lang="scss" scoped></style>
+<script lang="ts">
+import AppProvider from '@/components/AppProvider/index.vue';
+import { onLaunch, onShow, onHide } from '@dcloudio/uni-app';
+import { categoryInfo, categoryChild, customCategory } from './data.json';
+
+export default {
+    components: {
+        AppProvider
+    },
+    setup() {
+        const data = reactive({
+            category: [{}],
+            categoryChild: [{}],
+            customCategory: [{}]
+        });
+
+        onMounted(() => {
+            data.category = categoryInfo.category;
+            data.categoryChild = categoryChild;
+            data.customCategory = customCategory;
+            console.log("当前分类数据", data, customCategory);
+        });
+
+        const change = (index: any) => {
+            const list: any = data.category[index]
+            console.log("当前分类数据", list, index);
+            data.categoryChild = [].concat(list?.children as any);
+        };
+        const onChange = () => {
+            console.log("当前分类数据");
+        }
+        const changeCustom = () => {
+            console.log('点击分类数据')
+        };
+        return {
+            onChange,
+            change,
+            changeCustom,
+            ...toRefs(data)
+        }
+    }
+};
+</script>
