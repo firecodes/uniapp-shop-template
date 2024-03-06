5369279a6f22a519c9543397bad55b822929ca65
firecodes <384502958@qq.com>
2024-03-06 10:50:19 +0800

    update

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,95 +1,151 @@
 <script setup lang="ts">
-import BasicButton from '@/components/BasicButton/index.vue';
 import AppProvider from '@/components/AppProvider/index.vue';
-import { CURRENT_PLATFORM, PLATFORMS } from '@/enums/platformEnum';
-import { judgePlatform } from '@/utils/platform';
-import Iconify from '@/components/Iconify/index.vue';
-import { getEnvValue } from '@/utils/env';
-
-const appTitle = getEnvValue<string>('VITE_APP_TITLE');
-
-const title = ref(appTitle);
-
-const platform = CURRENT_PLATFORM;
-
-const isVue3 = judgePlatform(PLATFORMS.VUE3);
-
 const router = useRouter();
-const handleGetStarted = () => {
-    router.pushTab({ path: '/pages/demo/index' });
-};
+const my = (() => {
+    const viewScrollTop = 0
+    const upper = () => {
+    }
+    const viewscroll = () => {
+    }
+    const lower = () => {
+    }
+    return { viewScrollTop, upper, viewscroll, lower }
+})()
+
 </script>
 
 <template>
     <AppProvider>
-        <view class="index">
+        <view class="home">
+            <view class="swipe-box">
+                <view class="head">
+                    <image class="headLogo" style="height: 32px;" src="/static/images/avatar.png" />
+                    <view class="search">
+                        <view class="left">
+                            <view class="u-icon u-icon--right">
+                                <text class="u-icon__icon uicon-search u-iconfont"
+                                    style="font-size: inherit; font-weight: normal; top: 0px;"></text>
+                            </view>
+                            <text>请输入游戏名称或商品标题</text>
+                        </view>
+                    </view>
+                    <view class="cust-mini">
+                        <image style="height: 26px;" src="/static/images/avatar.png" />
+                        <image class="help" style="height: 26px;" src="/static/images/help.png" />
+                    </view>
+                </view>
+            </view>
+
+            <scroll-view :scroll-top="my.viewScrollTop" scroll-y="true" class="my-scroll" @scrolltoupper="my.upper"
+                @scrolltolower="my.lower" @scroll="my.viewscroll">
+                <view class="page" style="position: initial;">
+
+                    <view class="wrapper">
+                        <view class="swipe-box">
+                            <view class="function">
+                                <view class="function-wrapper">
+                                    <view class="item">
+                                        游戏交易
+                                    </view>
+                                    <view class="item">
+                                        账号估价
+                                    </view>
+                                    <view class="item">
+                                        中介担保
+                                    </view>
+                                    <view class="item">
+                                        账号回收
+                                    </view>
+                                </view>
+                            </view>
+                            <view class="swiper-bulletin-containt">
+                                <swiper class="swiper" :indicator-dots="true" circular :autoplay="true">
+                                    <swiper-item>
+                                        <view class="swiper-item uni-bg-red">A</view>
+                                    </swiper-item>
+                                    <swiper-item>
+                                        <view class="swiper-item uni-bg-green">B</view>
+                                    </swiper-item>
+                                    <swiper-item>
+                                        <view class="swiper-item uni-bg-blue">C</view>
+                                    </swiper-item>
+                                </swiper>
+                            </view>
+                            <view class="bulletin">
+                                <image class="help" style="height: 26px;" src="/static/images/help.png" />
+                                <view class="bulletinText">
+                                </view>
+                                <view class="bulletinMore">
+                                    <text>更多</text>
+                                    <image class="help" style="height: 26px;" src="/static/images/help.png" />
+                                </view>
+                            </view>
+                        </view>
+                        <view class="related">
+                            <view class="new_relatedCards leftTop">
+                                <view class="item"> 游戏交易 </view>
+                                <view class="item"> 账号估价 </view>
+                            </view>
+                            <view class="new_relatedCards rightTop">
+                                <view class="item"> 游戏交易 </view>
+                                <view class="item"> 账号估价 </view>
+                            </view>
+                            <view class="new_relatedCards leftTop">
+                                <view class="item"> 游戏交易 </view>
+                                <view class="item"> 账号估价 </view>
+                            </view>
+                            <view class="new_relatedCards rightTop">
+                                <view class="item"> 游戏交易 </view>
+                                <view class="item"> 账号估价 </view>
+                            </view>
+                            <view class="new_relatedCards leftTop">
+                                <view class="item"> 游戏交易 </view>
+                                <view class="item"> 账号估价 </view>
+                            </view>
+                            <view class="new_relatedCards rightTop">
+                                <view class="item"> 游戏交易 </view>
+                                <view class="item"> 账号估价 </view>
+                            </view>
+                            <view class="new_relatedCards leftTop">
+                                <view class="item"> 游戏交易 </view>
+                                <view class="item"> 账号估价 </view>
+                            </view>
+                            <view class="new_relatedCards rightTop">
+                                <view class="item"> 游戏交易 </view>
+                                <view class="item"> 账号估价 </view>
+                            </view>
+                            <view class="new_relatedCards leftTop">
+                                <view class="item"> 游戏交易 </view>
+                                <view class="item"> 账号估价 </view>
+                            </view>
+                            <view class="new_relatedCards rightTop">
+                                <view class="item"> 游戏交易 </view>
+                                <view class="item"> 账号估价 </view>
+                            </view>
+                            <view class="new_relatedCards leftTop">
+                                <view class="item"> 游戏交易 </view>
+                                <view class="item"> 账号估价 </view>
+                            </view>
+                            <view class="new_relatedCards rightTop">
+                                <view class="item"> 游戏交易 </view>
+                                <view class="item"> 账号估价 </view>
+                            </view>
+                            <!-- <view class="new_relatedCards leftTop">
+                                <view class="item"> 游戏交易 </view>
+                                <view class="item"> 账号估价 </view>
+                            </view>
+                            <view class="new_relatedCards rightTop">
+                                <view class="item"> 游戏交易 </view>
+                                <view class="item"> 账号估价 </view>
+                            </view> -->
+                        </view>
+                    </view>
 
 
-            <view style="padding: 20px;">
-                <!-- 以下形式在微信小程序会无效，APP和H5有效  -->
-                <u-button class="custom-style" text="雪月夜"></u-button>
-                <up-button type="primary" text="确定"></up-button>
-                <up-button type="primary" :plain="true" text="镂空"></up-button>
-                <up-button type="primary" :plain="true" :hairline="true" text="细边"></up-button>
-                <u-icon name="photo" color="#2979ff" size="28"></u-icon>
-                <up-image src="https://cdn.uviewui.com/uview/album/1.jpg" mode="widthFix"></up-image>
-                <up-text type="primary" text="主色"></up-text>
-                <up-text type="error" text="错误"></up-text>
-                <up-text type="success" text="成功"></up-text>
-                <up-text type="warning" text="警告"></up-text>
-                <up-text type="info" text="信息"></up-text>
-                <u-cell-group>
-                    <u-cell icon="setting-fill" title="个人设置"></u-cell>
-                    <u-cell icon="integral-fill" title="会员等级" value="新版本"></u-cell>
-                </u-cell-group>
-            </view>
-            <image class="logo" src="/static/svg/LOGO.svg" />
-            <view class="text-area">
-                <text class="">{{ title }}</text>
-            </view>
-            <view class="text-area">
-                <text class="">是否是Vue3: {{ isVue3 }}</text>
-            </view>
-            <view class="text-area">
-                <text class="_u_text-red">当前平台: {{ platform }}</text>
-            </view>
-            <BasicButton @click="handleGetStarted">Get Started → </BasicButton>
-            <view class="_u_text-red">uno css</view>
-            <Iconify icon="i-ph-anchor-simple-thin" size="65" />
-            <Iconify icon="i-system-uicons-book-text" />
-            <Iconify icon="i-system-uicons-battery-full" size="65" />
-            <Iconify icon="i-system-uicons-box-add" :size="65" />
-            <Iconify icon="i-system-uicons-bell-snooze" color="red" :size="65" />
-            <u-cell-group>
-                <u-cell icon="setting-fill" title="个人设置"></u-cell>
-                <u-cell icon="integral-fill" title="会员等级" value="新版本"></u-cell>
-            </u-cell-group>
+                </view>
+            </scroll-view>
         </view>
     </AppProvider>
 </template>
 
-<style lang="scss">
-.content {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-}
-
-.logo {
-    height: 200rpx;
-    width: 200rpx;
-    margin: 280rpx auto 50rpx;
-}
-
-.text-area {
-    display: flex;
-    justify-content: center;
-    margin-bottom: 60rpx;
-}
-
-.title {
-    font-size: 36rpx;
-    color: #8f8f94;
-}
-</style>
+<style lang="scss"></style>
